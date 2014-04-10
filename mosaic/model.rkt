0ba7903575363a3da5ad27eaeb742dde5bb1cabe
Konrad Hinsen <konrad.hinsen@fastmail.net>
2014-04-10 14:03:33 +0200

    Explicit list of provided symbols in mosaic/model

--- mosaic/model.rkt
+++ mosaic/model.rkt
@@ -1,6 +1,31 @@
 #lang racket
 
-(provide (all-defined-out))
+(provide atom-descr fragment-descr polymer-descr
+         node atom fragment universe configuration
+         make-fragment make-universe make-configuration
+         make-data-item
+         define-fragment define-polymer
+
+         Ac Ag Al Am Ar As At Au
+         B Ba Be Bh Bi Bk Br
+         C Ca Cd Ce Cf Cl Cm
+         Co Cn Cr Cs Cu
+         D Db Ds Dy
+         Er Es Eu
+         F Fe Fm Fr
+         Ga Gd Ge
+         H He Hf Hg Ho Hs
+         I In Ir
+         K Kr
+         La Li Lr Lu
+         Md Mg Mn Mo Mt
+         N Na Nb Nd Ne Ni No Np
+         O Os
+         P Pa Pb Pd Pm Po Pr Pt Pu
+         Ra Rb Re Rf Rg Rh Rn Ru
+         S Sb Sc Se Sg Si Sm Sn Sr
+         Ta Tb Tc Te Th Ti Tl Tm
+         U V W Xe Y Yb Zn Zr)
 
 (require (prefix-in interface:  "interface.rkt")
          (for-syntax syntax/parse)
